1b227d81f8414a6a4baa9acd37c0fcc086e8192f
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-27 12:56:11 +0300

    views

--- todoprj/todoapp/views.py
+++ todoprj/todoapp/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'todoapp/todo.html', {})
+
+def register(request):
+    return render(request, 'todoapp/register.html', {})
+
+def loginpage(request):
+    return render(request, 'todoapp/login.html', {})
+

ba3367b9296e0657263645f2807a6e4cb071e68a
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-27 15:41:32 +0300

    auth

--- todoprj/todoapp/views.py
+++ todoprj/todoapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request, 'todoapp/todo.html', {})

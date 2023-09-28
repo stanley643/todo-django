df342cd551b28540f82b2ca024e404fbeeac0cb0
Stanley643 <nmurigi@kabarak.ac.ke>
2023-09-28 22:30:51 +0300

    models

--- todoprj/todoapp/models.py
+++ todoprj/todoapp/models.py
@@ -4,5 +4,8 @@ from django.contrib.auth.models import User
 
 class todo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=1000)
+    todo_name = models.CharField(max_length=1000)
     status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.todo_name
